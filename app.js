84c97215e97721cb98ca6fe5b9afa8f1c7fb60c5
shccgxqp <shccgxqp@gmail.com>
2023-10-01 20:09:09 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const handlebars = require('express-handlebars') // 引入 express-handlebars
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport') //  引入 passport
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const routes = require('./routes')
+const { getUser } = require('./helpers/auth-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
@@ -24,6 +26,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 

29a03e23c1749aa07ef5710274f6c9c91260abd9
shccgxqp <shccgxqp@gmail.com>
2023-10-01 19:12:33 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const router = express.Router()
 
 //  新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
-
-//  新增
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router

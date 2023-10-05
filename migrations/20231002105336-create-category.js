1c91d14d914aa29492205b308e82a7f0ddf74e32
shccgxqp <shccgxqp@gmail.com>
2023-10-05 13:23:44 +0800

    fix migrations

--- migrations/20231002105336-create-category.js
+++ migrations/20231002105336-create-category.js
@@ -1,12 +1,27 @@
 'use strict'
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Restaurants', 'image', {
-      type: Sequelize.STRING
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Restaurants', 'image')
+    await queryInterface.dropTable('Categories')
   }
 }

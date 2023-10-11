4d2e3fab16a39e1c02e5f971a9ad18016b56d348
shccgxqp <shccgxqp@gmail.com>
2023-10-11 13:29:39 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)

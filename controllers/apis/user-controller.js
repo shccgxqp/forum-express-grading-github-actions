1ba6daea103eac94c248f8789bfbeacf1ac014e4
shccgxqp <shccgxqp@gmail.com>
2023-10-13 15:51:59 +0800

    feat: signup & modify api unauthorize response

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,9 +1,14 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+const db = require('../../models')
+const { User } = db
+
 const userController = {
   signIn: (req, res, next) => {
     try {
       const userData = req.user.toJSON()
-      delete userData.password // 新增這裡，刪除密碼
+      delete userData.password
 
       const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
       res.json({
@@ -13,6 +18,43 @@ const userController = {
           user: userData
         }
       })
+    } catch (err) {
+      next(err)
+    }
+  },
+  signUp: (req, res, next) => {
+    try {
+      if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            res.status(400).json({
+              status: 'error',
+              message: 'Email already exists!'
+            })
+          } else {
+            return bcrypt.hash(req.body.password, 10)
+              .then(hash => User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+              }))
+              .then(user => {
+                const userData = user.toJSON()
+                delete userData.password
+
+                const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+                res.json({
+                  status: 'success',
+                  data: {
+                    token,
+                    user: userData
+                  }
+                })
+              })
+          }
+        })
     } catch (err) {
       next(err)
     }
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -11,6 +11,10 @@ const userController = require('../../controllers/apis/user-controller')
 
 router.use('/admin', authenticated, authenticatedAdmin, admin) // 修改，後台路由加入 authenticated, authenticatedAdmin
 router.get('/restaurants', authenticated, restController.getRestaurants) // 修改，前台路由加入 authenticated
+
+router.post('/signup', userController.signUp)
+
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
 router.use('/', apiErrorHandler)
 module.exports = router
